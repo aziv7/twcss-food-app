e859e69fa45f4267bc58c3be399834bae4f173f3
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-07-01 21:23:24 +0100

    header twcss fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { Header } from './components';
-
+import { CreateItem, Header, MainContainer } from './components';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className='w-screen h-auto flex flex-col'>
+    <div className='w-screen h-auto flex flex-col bg-primary'>
       <Header />
+      <main className='w-full p-8 mt-24'>
+        <Routes>
+          <Route path='/new-item' element={<CreateItem />} />
+          <Route path='/' element={<MainContainer />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Logo from '../img/logo.png';
+import Avatar from '../img/avatar.png';
+import { FaShoppingCart } from 'react-icons/fa';
 const Header = () => {
   return (
     <div className='fixed w-screen z-50  p-6 px-16  '>
@@ -8,7 +10,7 @@ const Header = () => {
           <img src={Logo} alt='logo' className='w-10 object-cover' />
           <p className='text-headingColor text-xl font-bold'>City</p>
         </div>
-        <div>
+        <div className='flex items-center gap-8'>
           <ul className='flex items-center gap-8 '>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
               Home
@@ -22,7 +24,19 @@ const Header = () => {
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
               Service
             </li>
-          </ul>
+          </ul>{' '}
+          <div className='relative '>
+            <FaShoppingCart className='text-textColor text-3xl cursor-pointer'></FaShoppingCart>
+            <div className='w-5 h-5 rounded-full text-sm bg-cartNumBg  absolute font-semibold  -right-3 -top-2 text-white text-center'>
+              3
+            </div>{' '}
+          </div>
+          <img
+            loading='lazy'
+            className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl'
+            src={Avatar}
+            alt='user profile picture'
+          />
         </div>
       </div>
       <div className='flex md:hidden w-full h-full '></div>
